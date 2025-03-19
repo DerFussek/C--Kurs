82dbf196e81d41241d68cafe760ea5cf4a0cfdc6
DerFussek <tildussel@outlook.de>
2025-03-19 21:21:12 +0100

    Fehlerbehebung

--- Zeiger und Referenzen/Pointer.cc
+++ Zeiger und Referenzen/Pointer.cc
@@ -5,11 +5,11 @@ int main() {
 
     int number = 5;
     std::cout << "Value of Number: " << number << std::endl;
-    std::cout << "Adresse of Number: " << &number << std:endl;
+    std::cout << "Adresse of Number: " << &number << std::endl;
 
     int *p = &number;
     std::cout << "Value of p: " << p << std::endl;
-    std::cout << "Adresse of p: " << &p << std:endl;
+    std::cout << "Adresse of p: " << *p << std::endl;
 
 
     return 0;
