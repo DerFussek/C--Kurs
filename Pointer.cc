d1ad3ea996344a2944c590d5626b89188e357bc4
DerFussek <tildussel@outlook.de>
2025-03-19 20:54:28 +0100

    Add files via upload

---
+++ Pointer.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdint>
+
+int main() {
+
+    int number = 5;
+    std::cout << "Value of Number: " << number << std::endl;
+    std::cout << "Adresse of Number: " << &number << std:endl;
+
+    return 0;
+}
